6ac6ce9bab64daa3d68a89f57c83de5832948032
vadikk <vadtemplar@gmail.com>
2017-10-27 19:20:08 +0300

    first

---
+++ Assets/GameManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject[] images;
+    public GameObject next;
+    public GameObject Menu;
+
+    // Use this for initialization
+    void Start () {
+        images[0].gameObject.SetActive(true);
+        Menu.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (images[2].activeInHierarchy)
+        {
+            next.SetActive(false);
+            Menu.SetActive(true);
+
+        }
+
+    }
+
+    public void ChangeImage()
+    {
+
+        for (int i = 0; i < images.Length; i++)
+        {
+            if (images[i].activeInHierarchy)
+            {
+                images[i].gameObject.SetActive(false);
+                images[i+1].gameObject.SetActive(true);
+                return;
+            }
+
+            print(i);
+        }
+    }
+
+  public void GoToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+
+}
---
+++ Assets/Textures/LevelManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    public float loadnextLevel;
+    // Use this for initialization
+    void Start () {
+        Invoke("LoadLevel", loadnextLevel);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void LoadMain()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}
